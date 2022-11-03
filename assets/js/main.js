3165d875135b48c4f6c374396f4651129d45146e
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-11-02 20:44:37 -0700

    fdfd

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+jQuery(function ($) {
+
+    $(".galleryCar").owlCarousel({
+        nav: true,
+        loop: true,
+        dots: true,
+        center: true,
+        items: 1,
+        navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
+        margin: 10,
+        stagePadding: 100,
+        rtl: true,
+        responsive: {
+            0: {
+                stagePadding: 0,
+            },
+            800: {
+                stagePadding: 100,
+            }
+        }
+    });
+$(".ourClientCar").owlCarousel({
+    nav: true,
+    loop: false,
+    items: 2,
+    rtl: true,
+    navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
+    margin: 130,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 0
+        },
+
+        700: {
+            items: 2,
+        }
+    }
+
+});
+$(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+//add fix header when scroll down and remove when back up
+    if (scrollTop > $('header').height()) {
+      $('header').addClass('scrollNav');
+
+
+    }
+
+    else {
+      $('header').removeClass('scrollNav');
+    }
+
+  });
+
+
+});
+
